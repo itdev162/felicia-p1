2e46110ed9c9ace4de20ebcf525a7332e45a275b
Felicia <feliciajarosinski@gmail.com>
2020-10-04 18:13:02 -0500

    added projects solutions and aspnet

---
+++ API/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+/* using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Application.Posts;
+
+using Domain;
+
+using MediatR;
+
+namespace API.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public PostsController(IMediator mediator) => this.mediator = mediator;
+
+        public async Task<ActionResult<List<Post>>> List()
+        {
+            return await this.mediator.Send(new List.Query());
+        }
+    }
+} */
